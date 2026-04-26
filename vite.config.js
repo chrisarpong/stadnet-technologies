eabed2601e9b377817e13d9a67c37d285551ebb4
Bryte007 <koffie.hateka@gmail.com>
2026-04-26 11:36:03 +0000

    Revert "Revert "CONVERSION""

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,27 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (!id.includes('node_modules')) {
+            return undefined
+          }
+
+          if (id.includes('@emailjs')) {
+            return 'emailjs'
+          }
+
+          if (id.includes('react-icons')) {
+            return 'icon-vendor'
+          }
+
+          return undefined
+        }
+      }
+    }
+  },
   server: {
     host: true, // Expose to local network
     port: 5173,
